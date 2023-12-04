e67b129aba1e6d6ca6963a37b055d933ea40c21e
vishwaa <vishwaams.03@gmail.com>
2023-12-04 10:40:29 +0530

    Next Smallest Element 2

---
+++ src/NextSmallerElement2.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextSmallerElement2 {
+    public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && st.peek() >= nums[i]) {
+                st.pop();
+            }
+            if (!st.isEmpty()) nge[i] = st.peek();
+            else nge[i] = -1;
+            st.add(nums[i]);
+        }
+        return nge;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{34, 35, 27, 42, 5, 28, 39, 20, 28};
+        System.out.println(Arrays.toString(nextGreaterElements(arr)));
+    }
+
+}
